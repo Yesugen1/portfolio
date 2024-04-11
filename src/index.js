c39dc83b003466431e8a30094d532e278d877249
Yesugen Ankhbayar <yesugenankhbayar@yesugens-air.mynetworksettings.com>
2024-04-10 17:07:44 -0700

    sumbit button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 function sendMail() {
-  let parms = {
-    name: document.getElementById("nameHere").value,
-    email: document.getElementById("emailHere").value,
-    message: document.getElementById("messageHere").value,
-  };
-  emailjs
-    .send("service_r41nfvj", "template_rxjll6f", parms)
-    .then(alert("Email Sent"));
+  // Get input values
+  let name = document.getElementById("nameHere").value;
+  let email = document.getElementById("emailHere").value;
+  let message = document.getElementById("messageHere").value;
+
+  // Check if all input fields are filled
+  if (name && email && message) {
+    // If all fields are filled, proceed to send the email
+    let parms = {
+      name: name,
+      email: email,
+      message: message,
+    };
+    emailjs
+      .send("service_r41nfvj", "template_rxjll6f", parms)
+      .then(function () {
+        alert("Email Sent");
+      })
+      .catch(function (error) {
+        console.error("Error sending email: ", error);
+      });
+  } else {
+    // If any field is empty, show an alert to the user
+    alert("Please fill in all fields before sending the email.");
+  }
 }
